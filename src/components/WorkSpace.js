5a9b07b024a4c9a65a10de523bbb94f0e180c7e4
Dmitry Borok <dmitry.borok@gmail.com>
2023-09-07 02:22:22 +0300

    feat: side navbar

--- src/components/WorkSpace.js
+++ src/components/WorkSpace.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import {Box} from "@mui/material"
-import SideNavBar from './appBar/SideNavBar'
+import SideNavBar from './sideBar/SideNavBar'
 import Board from './board/Board'
-import AppBarHeader from './appBar/AppBar'
+import AppHeader from './appBar/AppHeader'
 import { inject, observer } from 'mobx-react'
 
 const WorkSpace = inject("usersTable", "boardsTable")(observer((props) => {
   return (
-   <Box>
-        <AppBarHeader/>
-        <SideNavBar />
-        <Board boardId={props.boardsTable.currentId}/>
+    <Box sx={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "start"}}>
+        <Box sx={{position: "absolute", left: "0px", top: "300px", zIndex: "-1"}}>test</Box>
+        <AppHeader/>
+        <Box sx={{height: "100%", display: "grid", gridTemplateColumns: "1fr 9fr"}}>
+          <SideNavBar />
+          <Board boardId={props.boardsTable.currentId}/>
+        </Box>
     </Box>
-  )
+        )
 }))
 
 export default WorkSpace;
