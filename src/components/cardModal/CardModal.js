0294e1d6393ea3b0aad1250f9c2f56968dff23c0
s.tolstov <sergeymtolstov35@gmail.com>
2023-09-14 17:33:23 +0300

    feat: style fixes

--- src/components/cardModal/CardModal.js
+++ src/components/cardModal/CardModal.js
@@ -1,129 +1,154 @@
-import {inject, observer} from "mobx-react";
+import { inject, observer } from "mobx-react";
 import {
-    Box, IconButton, InputAdornment,
-    Modal,
-    TextField,
+  Box,
+  IconButton,
+  InputAdornment,
+  Modal,
+  TextField,
+  Typography,
 } from "@mui/material";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CommentsList from "./CommentsList";
-import AddCommentIcon from '@mui/icons-material/AddComment';
+import AddCommentIcon from "@mui/icons-material/AddComment";
 
 const CardModal = inject(
-    "modalStateStore",
-    "cardsTable",
-    "commentsTable",
-    "usersTable"
+  "modalStateStore",
+  "cardsTable",
+  "commentsTable",
+  "usersTable"
 )(
-    observer((props) => {
-        const {open, toggle} = props.modalStateStore;
+  observer((props) => {
+    const { open, toggle } = props.modalStateStore;
 
-        const [currentCard, setCurrentCard] = useState(props.cardsTable.getItemById(props.modalStateStore.currentCardId));
-        const [newComment, setNewComment] = useState("")
+    const [currentCard, setCurrentCard] = useState(
+      props.cardsTable.getItemById(props.modalStateStore.currentCardId)
+    );
+    const [newComment, setNewComment] = useState("");
 
-        return (
-            <Modal
-                open={open}
-                onClose={() => {
-                    props.cardsTable.editItem({...currentCard})
-                    toggle(props.modalStateStore.currentCardId);
+    return (
+      <Modal
+        open={open}
+        onClose={() => {
+          props.cardsTable.editItem({ ...currentCard });
+          toggle(props.modalStateStore.currentCardId);
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            backgroundColor: "shades.main",
+            boxShadow: 24,
+            p: 4,
+            m: 4,
+            borderRadius: "5px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              gap: "10px",
+              flexDirection: "column",
+              width: "48%",
+              backgroundColor: "blue.contrastText",
+              borderRadius: "5px",
+              p: 4,
+            }}
+          >
+            <Box sx={{ display: "flex", gap: "10px" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  width: "30%",
+                  height: "100%",
+                  border: "1px solid",
+                  borderColor: "shades.main",
+                  borderRadius: "5px",
                 }}
-            >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        backgroundColor: "shades.main",
-                        boxShadow: 24,
-                        p: 4,
-                        m: 4,
-                        borderRadius: "5px",
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: "flex",
-                            gap: "10px",
-                            flexDirection: "column",
-                            width: "48%",
-                            backgroundColor: "blue.contrastText",
-                            borderRadius: "5px",
-                            p: 4,
-                        }}
-                    >
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Title"
-                            multiline
-                            rows={3}
-                            value={currentCard.name}
-                            onChange={(e) => setCurrentCard({...currentCard, name: e.target.value})}
-                            sx={{
-                                height: "100%",
-                                backgroundColor: "transparent.light",
-                                boxShadow: 2,
-                            }}
-                        />
+              >
+                {/* <Typography>{currentCard.createdAt}</Typography>
+                <Typography>{props.usersTable.getItemById(currentCard.createdBy)}</Typography> */}
+              </Box>
+              <TextField
+                id="outlined-multiline-static"
+                label="Title"
+                multiline
+                rows={3}
+                value={currentCard.name}
+                onChange={(e) =>
+                  setCurrentCard({ ...currentCard, name: e.target.value })
+                }
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  backgroundColor: "transparent.light",
+                  boxShadow: 2,
+                }}
+              />
+            </Box>
 
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Description"
-                            multiline
-                            rows={18}
-                            value={currentCard.description}
-                            onChange={(e) => setCurrentCard({...currentCard, description: e.target.value})}
-                            sx={{
-                                height: "100%",
-                                backgroundColor: "transparent.light",
-                                boxShadow: 2,
-                            }}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "10px",
-                            width: "48%",
-                            backgroundColor: "blue.contrastText",
-                            borderRadius: "5px",
-                            p: 4,
-                        }}
+            <TextField
+              id="outlined-multiline-static"
+              label="Description"
+              multiline
+              rows={18}
+              value={currentCard.description}
+              onChange={(e) =>
+                setCurrentCard({ ...currentCard, description: e.target.value })
+              }
+              sx={{
+                height: "100%",
+                backgroundColor: "transparent.light",
+                boxShadow: 2,
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              width: "48%",
+              backgroundColor: "blue.contrastText",
+              borderRadius: "5px",
+              p: 4,
+            }}
+          >
+            <CommentsList cardId={props.modalStateStore.currentCardId} />
+            <TextField
+              sx={{
+                backgroundColor: "transparent.contrastText",
+              }}
+              variant="outlined"
+              placeholder="Enter your comment:"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      disabled={newComment === ""}
+                      onClick={() => {
+                        props.commentsTable.addComment(
+                          newComment,
+                          props.usersTable.currentItem.id,
+                          props.modalStateStore.currentCardId
+                        );
+                        setNewComment("");
+                      }}
+                      edge="end"
                     >
-                        <CommentsList cardId={props.modalStateStore.currentCardId}/>
-                        <TextField
-                            sx={{
-                                backgroundColor: "transparent.contrastText",
-                            }}
-                            variant="outlined"
-                            placeholder="Enter your comment:"
-                            value={newComment}
-                            onChange={(e) => setNewComment(e.target.value)}
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            disabled={newComment === ""}
-                                            onClick={
-                                                () => {
-                                                    props.commentsTable.addComment(
-                                                        newComment,
-                                                        props.usersTable.currentItem.id,
-                                                        props.modalStateStore.currentCardId,
-                                                    )
-                                                    setNewComment("")
-                                                }
-                                            } edge="end">
-                                            <AddCommentIcon/>
-                                        </IconButton>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Box>
-                </Box>
-            </Modal>
-        );
-    })
+                      <AddCommentIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </Box>
+      </Modal>
+    );
+  })
 );
 
 export default CardModal;
