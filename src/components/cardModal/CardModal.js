0ac5eab52fae7f24582ea68a419a2b451ae3eb23
s.tolstov <sergeymtolstov35@gmail.com>
2023-09-18 17:12:05 +0300

    fix: cardModal styles + scroll on change

--- src/components/cardModal/CardModal.js
+++ src/components/cardModal/CardModal.js
@@ -1,151 +1,162 @@
-import {inject, observer} from "mobx-react";
+import { inject, observer } from "mobx-react";
 import {
-    Box, IconButton, InputAdornment,
-    Modal,
-    TextField,
+  Box,
+  IconButton,
+  InputAdornment,
+  Modal,
+  TextField,
 } from "@mui/material";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CommentsList from "./CommentsList";
-import AddCommentIcon from '@mui/icons-material/AddComment';
+import AddCommentIcon from "@mui/icons-material/AddComment";
 
 const CardModal = inject(
-    "modalStateStore",
-    "cardsTable",
-    "commentsTable",
-    "usersTable"
+  "modalStateStore",
+  "cardsTable",
+  "commentsTable",
+  "usersTable"
 )(
-    observer((props) => {
-        const {open, toggle} = props.modalStateStore;
+  observer((props) => {
+    const { open, toggle } = props.modalStateStore;
 
-        const [currentCard, setCurrentCard] = useState(props.cardsTable.getItemById(props.modalStateStore.currentCardId));
-        const [newComment, setNewComment] = useState("")
+    const [currentCard, setCurrentCard] = useState(
+      props.cardsTable.getItemById(props.modalStateStore.currentCardId)
+    );
+    const [newComment, setNewComment] = useState("");
 
-        const handleOK = function() {
-            props.commentsTable.addComment(
-                newComment,
-                props.usersTable.currentItem.id,
-                props.modalStateStore.currentCardId,
-            )
-            setNewComment("");
-        }
+    const handleOK = function () {
+      props.commentsTable.addComment(
+        newComment,
+        props.usersTable.currentItem.id,
+        props.modalStateStore.currentCardId
+      );
+      setNewComment("");
+    };
 
-        const handleCancel = function() {
-            setNewComment("");
-        }
+    const handleCancel = function () {
+      setNewComment("");
+    };
 
-        const handleKeyDown = (event) => {
-            if (event.key === 'Enter') {
-                handleOK();
-            } else if (event.key === 'Escape') {
-                handleCancel();
-            }
-        };
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        handleOK();
+      } else if (event.key === "Escape") {
+        handleCancel();
+      }
+    };
 
-        return (
-            <Modal
-                open={open}
-                onClose={() => {
-                    props.cardsTable.editItem({...currentCard})
-                    toggle(props.modalStateStore.currentCardId);
-                }}
-            >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-evenly',
-                        backgroundColor: "shades.main",
-                        boxShadow: 24,
-                        p: 4,
-                        m: 16,
-                        borderRadius: "5px",
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: "flex",
-                            gap: "10px",
-                            flexDirection: "column",
-                            width: "48%",
-                            backgroundColor: "blue.contrastText",
-                            borderRadius: "5px",
-                            p: 4,
-                        }}
+    return (
+      <Modal
+        open={open}
+        onClose={() => {
+          props.cardsTable.editItem({ ...currentCard });
+          toggle(props.modalStateStore.currentCardId);
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            backgroundColor: "shades.main",
+            boxShadow: 24,
+            p: 4,
+            margin: "5% 10%",
+            borderRadius: "5px",
+            height: "80%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              gap: "10px",
+              flexDirection: "column",
+              width: "48%",
+              backgroundColor: "blue.contrastText",
+              borderRadius: "5px",
+              p: 4,
+            }}
+          >
+            <TextField
+              id="outlined-multiline-static"
+              label="Title"
+              multiline
+              rows={3}
+              value={currentCard.name}
+              onChange={(e) =>
+                setCurrentCard({ ...currentCard, name: e.target.value })
+              }
+              color="shades"
+              sx={{
+                height: "100%",
+                backgroundColor: "transparent.light",
+                boxShadow: 2,
+              }}
+            />
+
+            <TextField
+              id="outlined-multiline-static"
+              label="Description"
+              multiline
+              rows={12}
+              value={currentCard.description}
+              onChange={(e) =>
+                setCurrentCard({ ...currentCard, description: e.target.value })
+              }
+              color="shades"
+              sx={{
+                height: "100%",
+                backgroundColor: "transparent.light",
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              width: "48%",
+              backgroundColor: "blue.contrastText",
+              borderRadius: "5px",
+              p: 4,
+            }}
+          >
+            <CommentsList cardId={props.modalStateStore.currentCardId} />
+            <TextField
+              sx={{
+                backgroundColor: "transparent.contrastText",
+              }}
+              variant="outlined"
+              placeholder="Enter your comment:"
+              value={newComment}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setNewComment(e.target.value)}
+              color="shades"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      disabled={newComment === ""}
+                      onClick={() => {
+                        props.commentsTable.addComment(
+                          newComment,
+                          props.usersTable.currentItem.id,
+                          props.modalStateStore.currentCardId
+                        );
+                        setNewComment("");
+                      }}
+                      edge="end"
                     >
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Title"
-                            multiline
-                            rows={3}
-                            value={currentCard.name}
-                            onChange={(e) => setCurrentCard({...currentCard, name: e.target.value})}
-                            sx={{
-                                height: "100%",
-                                backgroundColor: "transparent.light",
-                                boxShadow: 2,
-                            }}
-                        />
-
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Description"
-                            multiline
-                            rows={18}
-                            value={currentCard.description}
-                            onChange={(e) => setCurrentCard({...currentCard, description: e.target.value})}
-                            sx={{
-                                height: "100%",
-                                backgroundColor: "transparent.light",
-                                boxShadow: 2,
-                            }}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "10px",
-                            width: "48%",
-                            backgroundColor: "blue.contrastText",
-                            borderRadius: "5px",
-                            p: 4,
-                        }}
-                    >
-                        <CommentsList cardId={props.modalStateStore.currentCardId}/>
-                        <TextField
-                            sx={{
-                                backgroundColor: "transparent.contrastText",
-                            }}
-                            variant="outlined"
-                            placeholder="Enter your comment:"
-                            value={newComment}
-                            onKeyDown={handleKeyDown}
-                            onChange={(e) => setNewComment(e.target.value)}
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            disabled={newComment === ""}
-                                            onClick={
-                                                () => {
-                                                    props.commentsTable.addComment(
-                                                        newComment,
-                                                        props.usersTable.currentItem.id,
-                                                        props.modalStateStore.currentCardId,
-                                                    )
-                                                    setNewComment("")
-                                                }
-                                            } edge="end">
-                                            <AddCommentIcon/>
-                                        </IconButton>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Box>
-                </Box>
-            </Modal>
-        );
-    })
+                      <AddCommentIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </Box>
+      </Modal>
+    );
+  })
 );
 
 export default CardModal;
--- src/components/cardModal/CommentsList.js
+++ src/components/cardModal/CommentsList.js
@@ -1,39 +1,53 @@
-import Comment from './Comment'
-import {inject, observer} from "mobx-react";
-import {Box} from "@mui/material";
+import Comment from "./Comment";
+import { inject, observer } from "mobx-react";
+import { Box } from "@mui/material";
+import { useRef, useEffect } from "react";
 
+const CommentsList = inject("commentsTable")(
+  observer((props) => {
+    const messagesEndRef = useRef(null);
 
-const CommentsList = inject(
-    "commentsTable",
-)(
-    observer((props) => {
+    useEffect(() => {
+        console.log("hi")
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [props.commentsTable.data.length]);
 
-        return (
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "5px",
-                        borderRadius: "5px",
-                        backgroundColor: "transparent.light",
-                        boxShadow: 2,
-                        flexGrow: 1,
-                        overflow: "auto",
-                        p: 1,
-                    }}
-                    className="no-scrollbar"
-                >
-                    {[
-                        ...props.commentsTable.getCommentByCardId(props.cardId)
-                    ]
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "5px",
+          borderRadius: "5px",
+          backgroundColor: "transparent.light",
+          boxShadow: 2,
+          flexGrow: 1,
+          overflow: "auto",
+          p: 1,
+        }}
+        className="no-scrollbar"
+      >
+        {[...props.commentsTable.getCommentByCardId(props.cardId)]
 
-                        .sort((a, b) => a.created_at < b.created_at ? -1 : a.created_at > b.created_at ? 1 : 0)
-                        .map((comment) => (
-                            <Comment key={comment.id} commentId={comment.id} ownerComment={comment.user_id}/>
-                        ))}
-                </Box>
-        )
-    })
-)
+          .sort((a, b) =>
+            a.created_at < b.created_at
+              ? -1
+              : a.created_at > b.created_at
+              ? 1
+              : 0
+          )
+          .map((comment) => (
+            <Comment
+              key={comment.id}
+              commentId={comment.id}
+              ownerComment={comment.user_id}
+            />
+          ))}
+
+        <Box ref={messagesEndRef} style={{ float: "left", clear: "both" }} />
+      </Box>
+    );
+  })
+);
 
 export default CommentsList;
