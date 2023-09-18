a3dc49e67c8db120dc3980435a6cc70ca405851b
Dmitry Borok <dmitry.borok@gmail.com>
2023-09-18 23:46:22 +0300

    feat: customized scrollbars in Board and CardList

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -39,6 +39,21 @@ const Board = inject("cardListsTable")(
             height: "calc(100vh - 140px)",
             overflowX: "auto",
             padding: "20px 15px",
+            '&::-webkit-scrollbar': {
+              height: '0.5em',
+            },
+            '&::-webkit-scrollbar-track': {
+              boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+              webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: 'rgba(0,0,0,.1)',
+              outline: '1px solid slategrey'
+            },
+            '&::-webkit-scrollbar-track-piece': {
+              backgroundColor: 'rgba(0,0,0,.2)'
+            }
+          
           }}
         >
           <Box
--- src/components/board/CardList.js
+++ src/components/board/CardList.js
@@ -90,6 +90,21 @@ const CardList = inject(
             backgroundColor: "shades.main",
             borderRadius: "5px",
             display: isDragging ? "none" : "flex",
+            '&::-webkit-scrollbar': {
+              height: '0.5em',
+            },
+            '&::-webkit-scrollbar-track': {
+              boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+              webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: 'rgba(0,0,0,.1)',
+              outline: '1px solid slategrey'
+            },
+            '&::-webkit-scrollbar-track-piece': {
+              backgroundColor: 'rgba(0,0,0,.2)'
+            }
+
           }}
         >
           <Box
