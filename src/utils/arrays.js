a40f390b5f6e708ea0b919800ccdd9e5bb3f052c
Dmitry Borok <dmitry.borok@gmail.com>
2023-09-19 21:43:36 +0300

    fix: algorythmic bug in moveCard, moveCardList

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -4,3 +4,11 @@ export const getMaxObjectInArray = function(arr, key) {
       }, arr[0]);
         
 }
+
+export const getMinObjectInArray = function(arr, key) {
+  return arr.reduce((min, current) => {
+      return current[key] < min[key] ? current : min;
+    }, arr[0]);
+      
+}
+
